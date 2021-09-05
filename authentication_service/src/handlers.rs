4b1034ce5babef053050b66ed59d098bd1b15826
Brandsma <a.brandsma@protonmail.com>
2021-09-05 12:10:30 +0200

    Added user collection interface and capability to add users

--- authentication_service/src/handlers.rs
+++ authentication_service/src/handlers.rs
@@ -1,17 +1,44 @@
-use actix_web::Responder;
+use actix_web::{web, HttpResponse};
+use mongodb::{bson::doc};
+use serde::{Deserialize, Serialize};
+use log::info;
 
-pub async fn get_users() -> impl Responder {
-    format!("hello from get users")
+#[derive(Deserialize, Serialize)]
+pub struct User {
+    username: String,
+    password: String,
+    email: String,
 }
 
-pub async fn get_user_by_id() -> impl Responder {
-    format!("hello from get users by id")
+pub async fn get_users() -> HttpResponse {
+    HttpResponse::Ok().body("get user not implemented yet")
 }
 
-pub async fn add_user() -> impl Responder {
-    format!("hello from add user")
+pub async fn get_user_by_id() -> HttpResponse {
+    HttpResponse::Ok().body("get user by id not implemented yet")
 }
 
-pub async fn delete_user() -> impl Responder {
-    format!("hello from delete user")
+pub async fn add_user(user: web::Json<User>, data: web::Data<crate::AppState>) -> HttpResponse {
+    // Get the User Collection Interface
+    let user_service = &data.service_container.user;
+
+    // Create the new user
+    let new_user = doc! {
+        "username": &user.username,
+        "email": &user.email,
+        // TODO: Encrypt the password
+        "password": &user.password,
+    };
+
+    info!("inserted {}", new_user.get("username").unwrap());
+
+    // Insert new user into database
+    let _ = user_service.create(new_user.clone()).await.unwrap();
+
+    // Respond with Created code and JSON of new user
+    HttpResponse::Created().json(new_user)
+}
+
+pub async fn delete_user() -> HttpResponse {
+    HttpResponse::Ok().body("delete user not implemented yet")
 }
--- authentication_service/src/main.rs
+++ authentication_service/src/main.rs
@@ -1,38 +1,64 @@
+use actix_web::{middleware::Logger, web, App, HttpServer};
+use mongodb::{bson::doc, options::ClientOptions, Client};
+use users::UserService;
+use env_logger::Builder;
+use log::LevelFilter;
 
-use mongodb::bson::{self, doc, Bson};
-use actix_web::http::{header, Method, StatusCode};
-use actix_web::{
-    error, get, guard, middleware::Logger, web, App, Error, HttpRequest, HttpResponse,
-    HttpServer, Result,
-};
 
 mod handlers;
+mod users;
+
+pub struct ServiceContainer {
+    user: UserService,
+}
+
+impl ServiceContainer {
+    pub fn new(user: UserService) -> Self {
+        ServiceContainer { user }
+    }
+}
+
+pub struct AppState {
+    service_container: ServiceContainer,
+}
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=debug");
-    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+    Builder::new()
+        .filter(None, LevelFilter::Info)
+        .init();
 
-    let client_uri = "mongodb://main_admin:abc123@0.0.0.0:27017";
-    // A Client is needed to connect to MongoDB:
+    // env_logger::init();
+    // env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
 
-    // An extra line of code to work around a DNS issue on Windows:
+    // Parse your connection string into an options struct
+    let client_options =
+        ClientOptions::parse("mongodb://main_admin:abc123@0.0.0.0:27017")
+        .await
+        .unwrap();
+    let client = Client::with_options(client_options).unwrap();
 
-    // let options = ClientOptions::parse_with_resolver_config(&client_uri, ResolverConfig::cloudflare()).await?;
+    // Check if the mongo connection is successfull
+    let res = client
+        .database("admin")
+        .run_command(doc! {"ping": 1}, None)
+        .await
+        .unwrap();
+    println!("{}", res);
+    println!("Connected successfully.");
 
-    // let client = mongodb::Client::with_options(options);
-    // println!("Starting Authentication Service...");
-
-    // println!("Databases:");
-    // for name in client.list_database_names(None, None).await {
-    //     println!("- {}", name);
-
-    // }
-
+    let db = client.database("schro-database");
+    let user_collection = db.collection("users");
 
     // Start http server
     HttpServer::new(move || {
+
+        let service_container = ServiceContainer::new(UserService::new(user_collection.clone()));
         App::new()
+            .data(AppState {
+                service_container,
+            })
             .wrap(Logger::default())
             .route("/login", web::post().to(handlers::get_users))
             .route("/signup", web::post().to(handlers::add_user))
